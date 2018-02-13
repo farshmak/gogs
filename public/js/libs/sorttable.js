7282e9d82f8ceb379e58c09885540800c188bc93
Gogs <gogs@fake.local>
2018-02-13 19:42:42 +0300

    Added 'human sorting' for tables

--- public/js/libs/sorttable.js
+++ public/js/libs/sorttable.js
@@ -5,11 +5,35 @@ function sortTable(table, col, reverse) {
     reverse = -((+reverse) || -1);
     tr = tr.sort(function (a, b) { // sort rows
         return reverse // `-1 *` if want opposite order
+        	  * compareStrings(a.cells[col].textContent.trim(),
+                             b.cells[col].textContent.trim())            
+        /*
             * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
                 .localeCompare(b.cells[col].textContent.trim())
-               );
+               );               
+        */
     });
     for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+}
+
+function compareStrings(str1, str2) {
+	var rx = /([^\d]+|\d+)/ig;
+	var str1split = str1.match( rx );
+    var str2split = str2.match( rx );
+    if (null === str1split)
+        return 1;
+    if (null === str2split)
+        return -1;
+	for(var i=0, l=Math.min(str1split.length, str2split.length); i < l; i++) {
+		var s1 = str1split[i], 
+		    s2 = str2split[i];
+		if (s1 === s2) continue;
+		if (isNaN(+s1) || isNaN(+s2))
+			return s1 > s2 ? 1 : -1;
+		else
+			return +s1 - s2;		
+	}
+	return 0;
 }
 
 function makeSortable(table) {
